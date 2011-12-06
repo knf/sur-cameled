2524df1ce4259c6f2d0bf937cff3af629af034c7
Nicholas Klick <nicholas.klick@gmail.com>
2011-12-06 14:27:56 -0600

    adding core files2

--- example.rb
+++ example.rb
@@ -1,7 +1,6 @@
 Employee.all.each do |employee|
   surname = SurCameled::Surname.parse(employee.last_name)
   if surname
-    puts "UPDATING: #{employee.full_name} with #{surname}"
     employee.update_attributes(:last_name => surname)
   end
 end
