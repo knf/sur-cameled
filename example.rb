9886eb161789df9001d79b3e8be3e785561c97ce
Nicholas Klick <nicholas.klick@gmail.com>
2011-12-06 14:50:10 -0600

    better example2

--- example.rb
+++ example.rb
@@ -1,3 +1,7 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + "/surname.rb"
+
 %w(O'brian Maclaren Maclaughlin Maclea Maclean Mcgovern).each do |name|
   surname = SurCameled::Surname.parse(name)
   if surname
